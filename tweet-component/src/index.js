0abd421d2a6e1abc0949d8ff1275040c2b084d39
Nirmala Halappa <nirmala.halappa@adylic.com>
2020-06-02 08:16:30 +0100

    made components dynamic with props

--- tweet-component/src/index.js
+++ tweet-component/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-function Tweet(){
+function Tweet({tweet}){
     return(
         <div className = 'tweet' >
-            <Avatar/>
+            <Avatar hash = {tweet.gravatar}/>
             <div className = 'content'>
-                <Author/>
-                <Time/>
-                <Message/>
+                <Author author = {tweet.author}/>
+                <Time time = {tweet.timeStamp}/>
+                <Message text = {tweet.message}/>
                 <div className = 'buttons'>
                     <ReplyButton/>
-                    <ReTweetButton/>
-                    <LikeButton/>
+                    <ReTweetButton count = {tweet.retweets}/>
+                    <LikeButton count = {tweet.likes}/>
                     <MoreOptionsButton/>
                 </div>
             </div>
@@ -24,40 +24,65 @@ function Tweet(){
 const ReplyButton = () => (
     <button className = 'reply-button button' >reply</button>
 )
-const ReTweetButton = () => (
-    <button className = 'retweet-button button' >retweet</button>
+function getRetweetCount(count){
+    if(count > 0){
+        return <span className='retweet-count'>{count}</span>
+    }
+    return null;
+}
+const ReTweetButton = ({count}) => (
+
+   <span className = 'retweet-button'>
+        <button className = 'button' >retweet</button>
+        {getRetweetCount(count)}
+    </span>
 )
-const LikeButton = () => (
-    <button className = 'like-button button' >like</button>
+const LikeButton = ({count}) => (
+    <span className = 'like-button'>
+        <button className = 'button' >like</button>
+        {count > 0 && <span className='like-count'>{count}</span>}
+    </span>
 )
 const MoreOptionsButton = () => (
     <button className = 'more-options-button button' >more...</button>
 )
-function Message(){
-    return <div className = 'message'>Something great...</div>
+function Message({text}){
+    return <div className = 'message'>{text}</div>
 }
-function Time(){
-    return <span className = 'time'>3hrs ago</span>
+function Time({time}){
+    return <span className = 'time'>{time}</span>
 }
 
-function Author(){
+function Author({author}){
     return(
         <span className = 'author'>
-            <span className = 'name'>Nirmala Halappa</span>
-            <span className = 'handle'>@nhalappa</span>
+            <span className = 'name'>{author.name}</span>
+            <span className = 'handle'>{author.handle}</span>
         </span>
     )
 }
-function Avatar(){
+function Avatar({hash}){
     return(
         <img
         alt = 'avatar'
         className = 'avatar'
-        src = 'https://www.gravatar.com/avatar/nothing' />
+        src = {`https://www.gravatar.com/avatar/${hash}`} />
     )
 }
 
+const data = {
+    message: 'a tweet about learing react',
+    gravatar: 'e62685a54f531f03e0db79714b901d7c',
+    author: {
+        name: 'Nirmala Halappa',
+        handle: '@nhalappa'
+    },
+    likes: 25,
+    retweets: 0,
+    timeStamp: Date()
+}
+
 ReactDom.render(
-    <Tweet/>,
+    <Tweet tweet={data}/>,
     document.querySelector('#root')
 );
