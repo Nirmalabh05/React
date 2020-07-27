e493f3b03e5cc60f3b17786eba42c10adf86c0a9
Nirmala Halappa <nirmala.halappa@adylic.com>
2020-07-27 08:57:37 +0100

    Free code camp tutorial started

---
+++ code-test-01/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo(){
+    return(
+        <div>
+            <h1>Nirmala Halappa</h1>
+            <p>I am Front-End Developer.</p>
+            <ul>
+                <li>Switzerland</li>
+                <li>Gran Canaria</li>
+                <li>Teneriff</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
